204f5a1cd1befde490b568b9b94298fb113175d3
Cornum <129629510+Cornum@users.noreply.github.com>
2024-01-21 23:51:47 +0100

    Reupload last commit

--- VtM/app.js
+++ VtM/app.js
@@ -1,14 +1,34 @@
 //lorem code
 const sendBtn = document.getElementById('send')
-const inputVal = document.getElementById('playerName')
+const characteristicsPhysicalMax = document.getElementById('physical')
+const releaseBtn = document.getElementById('release')
 
+releaseBtn.onclick = function(){checkMax(characteristicsPhysicalMax)}
+sendBtn.onclick = function(){toggleTheme()}
 
-sendBtn.onclick = function(){toogleTheme()}
-
-function toogleTheme(){
+function toggleTheme(){
     const currentTheme = document.getElementById('themeStyles').getAttribute('href')
     const themeStyle = (currentTheme === 'styles.css') ? 'darkAgesStyles.css' : 'styles.css'
 
     document.getElementById('themeStyles').setAttribute('href', themeStyle)
 }
+function checkMax(Max){
+    switch(Number(Max.value)){
+        case 7: {
+            Max.setAttribute('style="background: #fefefe"')  //Find how to do it
+            break
+        }
+        case 5: {
+            
+        }
+        case 3: {
+            
+        }
+        default: {
+            toggleTheme()
+            break
+        }
+    }
+}
 // Write characteristics, skills... points logic
+
